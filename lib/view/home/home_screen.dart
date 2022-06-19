c1dd7ee77fe7039e5f9af78d352174d22efe375a
victor-onoja <odohvictor47@gmail.com>
2022-06-19 22:14:19 +0100

    home page ui initial commit

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,1 +1,107 @@
-//TODO: Implement the home screen here and create any folder needed for other screens
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    const pp = 'assets/profilepic.png';
+    const boy = 'assets/rafki.png';
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Text(
+            'Index',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+          actions: [
+            Container(
+              child: Image.asset(pp),
+            )
+          ],
+        ),
+        drawer: const Drawer(),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 60),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(child: Image.asset(boy)),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'What do you want to do today?',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 10),
+                    Text('Tap + to add your tasks')
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.yellow,
+          child: Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.calendar_month,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.access_alarm,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
